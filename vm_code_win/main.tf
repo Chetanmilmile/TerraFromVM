46f21ffd5bb2d545359200fed821c28540137ca9
chetan milmile <107764112+Chetanmilmile@users.noreply.github.com>
2023-10-18 12:36:42 +0530

    gs

--- vm_code_win/main.tf
+++ vm_code_win/main.tf
@@ -3,26 +3,33 @@ provider "azurerm" {
   features {}
 }
 
+
+
+resource "azurerm_resource_group" "rg1" {
+  name = "cloud"
+  location = "westus2"
+}
+
 # Create the first virtual network
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1"
+  name                = "shivani"
   address_space       = ["10.0.0.0/16"]
-  location            = "East US" # Change to your desired location
-  resource_group_name = "my-resource-group"
+  location            = "west us 1" # Change to your desired location
+  resource_group_name = azurerm_resource_group.rg1.name
 }
 
 # Create the second virtual network
 resource "azurerm_virtual_network" "vnet2" {
-  name                = "vnet2"
+  name                = "roshani"
   address_space       = ["10.1.0.0/16"]
-  location            = "West US" # Change to your desired location
-  resource_group_name = "my-resource-group"
+  location            = "West US 2" # Change to your desired location
+  resource_group_name = azurerm_resource_group.rg1.name
 }
 
 # Create virtual network peering from vnet1 to vnet2
 resource "azurerm_virtual_network_peering" "peer1_to_2" {
   name                      = "peer1_to_2"
-  resource_group_name       = "my-resource-group"
+  resource_group_name = azurerm_resource_group.rg1.name
   virtual_network_name      = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
   allow_virtual_network_access = true
@@ -31,7 +38,7 @@ resource "azurerm_virtual_network_peering" "peer1_to_2" {
 # Create virtual network peering from vnet2 to vnet1
 resource "azurerm_virtual_network_peering" "peer2_to_1" {
   name                      = "peer2_to_1"
-  resource_group_name       = "my-resource-group"
+  resource_group_name = azurerm_resource_group.rg1.name
   virtual_network_name      = azurerm_virtual_network.vnet2.name
   remote_virtual_network_id = azurerm_virtual_network.vnet1.id
   allow_virtual_network_access = true
