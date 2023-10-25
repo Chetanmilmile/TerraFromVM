6e11f915bfebc89207dc444d3e513c13b362e45e
chetan milmile <107764112+Chetanmilmile@users.noreply.github.com>
2023-10-25 12:47:22 +0530

    Update main.tf

--- new/main.tf
+++ new/main.tf
@@ -52,28 +52,28 @@ resource "azurerm_virtual_network" "rg2-vnet-1" {
   name = "SPOKE-app-vnet"
   location = azurerm_resource_group.rg2.location
   resource_group_name = azurerm_resource_group.rg2.name
-  address_space = ["192.168.0.0/24"]
+  address_space = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "rg2-subnet-1" {
   name = "SPOKE-app-subnet"
   resource_group_name = azurerm_resource_group.rg2.name
   virtual_network_name = azurerm_virtual_network.rg2-vnet-1.name
-  address_prefixes = ["192.168.0.0/25"]
+  address_prefixes = ["10.0.2.0/24"]
 }
 
 resource "azurerm_virtual_network" "rg2-vnet-2" {
   name = "SPOKE-DB-vnet"
   location = azurerm_resource_group.rg2.location
   resource_group_name = azurerm_resource_group.rg2.name
-  address_space = ["192.168.1.0/24"]
+  address_space = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "rg2-subnet-2" {
   name = "SPOKE-DB-subnet-1"
   resource_group_name = azurerm_resource_group.rg2.name
   virtual_network_name = azurerm_virtual_network.rg2-vnet-2.name
-  address_prefixes = ["192.168.1.0/24"]
+  address_prefixes = ["10.0.3.0/24"]
 }
 /* 
 resource "azurerm_virtual_network_peering" "vnet_peering" {
